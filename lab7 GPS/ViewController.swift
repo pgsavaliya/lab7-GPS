70a74259998d832d18e75828a480ba713838127e
Pgsavaliya <pgsavaliya01@gmail.com>
2024-03-14 11:57:42 -0400

    Initial Commit

---
+++ lab7 GPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab7 GPS
+//
+//  Created by Pavan savaliya on 2024-03-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lab7 GPSUITests/lab7_GPSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  lab7_GPSUITestsLaunchTests.swift
+//  lab7 GPSUITests
+//
+//  Created by Pavan savaliya on 2024-03-14.
+//
+
+import XCTest
+
+final class lab7_GPSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
